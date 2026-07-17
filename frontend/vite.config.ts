48520c8914c133f1578b65e5f7286e301ce635e8
individuocomunycorriente <danko.leiva@uc.cl>
2026-07-17 12:54:08 -0400

    feat/frontned

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // Permite que Docker acceda al servidor
+    port: 5173,
+    watch: {
+      usePolling: true, // Necesario en Linux/Docker para detectar cambios de archivos en volúmenes compartidos
+    },
+  },
+})
